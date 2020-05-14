de918a0aa3cdeec67c50aeeb7339e2e16f9127e6
Julia Hansi <ju851han@htwg-konstanz.de>
2020-05-14 17:15:53 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app') <!-- bedeutet dass eine exisitierende Template wird verwendet -->
+
+@section('content') <!-- Referenz damit Template Engine weiß, an welcher Stelle des Templates der u.a. Text eingefügt wird-->
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
