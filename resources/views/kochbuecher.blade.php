957ee22091df90f257e1efef5631242d8bfa2967
Julia Hansi <ju851han@htwg-konstanz.de>
2020-05-25 17:19:59 +0200

    add kochbuecher.blade.php - table for kochbuecher

---
+++ resources/views/kochbuecher.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <section>
+        <h2>Kochbücher</h2>
+        @php
+            $rows = DB::select('SELECT name,email FROM USERS');
+        @endphp
+
+        <table class="table">
+            <thead class="thead_orangered">
+            <tr>
+                <th>Kochbuch-Nr.</th>
+                <th>Name</th>
+            </tr>
+            </thead>
+            <tbody class="background2ndTR">
+            @foreach ($rows as $k)
+                <tr>
+                    <td> {{  $k->kid }} </td>
+                    <td> {{  $k->kname }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </section>
+@endsection
