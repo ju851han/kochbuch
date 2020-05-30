30cd61b603f733380a1565f5898ef29769f99b9c
Julia Hansi <ju851han@htwg-konstanz.de>
2020-05-30 22:02:14 +0200

    add routes for creating a new kochbuch

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Route::get('/users','UserController@index')->name('users');
 // Show all Kochbuecher
 Route::get('/kochbuecher','KochbuchController@index')->name('kochbuecher.index');
 
+// Add Kochbuch
+Route::get('/kochbuecher/create','KochbuchController@create')->name('kochbuecher.create');
+Route::post('/kochbuecher','KochbuchController@store')->name('kochbuecher.store');
 
 /****TODO Routes for Rezepte **************************************************************************/
 // Show all Rezepte
@@ -31,7 +34,7 @@ Route::get('/rezepte','RezeptController@index')->name('rezepte.index');
 // Show all Zutaten
 Route::get('/zutaten','ZutatenController@index')->name('zutaten.index');
 
-//Add Zutat
+// Add Zutat
 Route::get('/zutaten/create','ZutatenController@create')->name('zutaten.create');
 Route::post('/zutaten','ZutatenController@store')->name('zutaten.store');
 
