c614da578c574f75e33889dcae9d8d64d9be8b64
Julia Hansi <ju851han@htwg-konstanz.de>
2020-05-31 22:10:40 +0200

    add view for show a zutat

---
+++ resources/views/zutaten/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<h2>Zutat</h2>
+<table class="table">
+    <thead>
+    <tr>
+        <th>Zutaten Name</th>
+        <th>Kosten je Einheit</th>
+        <th>Mengeneinheit</th>
+        <th>Produktgruppe</th>
+    </tr>
+    </thead>
+    <tbody class="background2ndTR">
+          <tr>
+            <td> {{  $z->zName }} </td>
+            <td> {{  $z->mengeneinheit }}</td>
+            <td> {{  $z->kostenJeEinheit }}</td>
+            <td> {{  $z->produktgruppe }}</td>
+        </tr>
+    </tbody>
+</table>
+@endsection
