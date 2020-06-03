dc180b203c1d65adca471af597c49e7aa8f47451
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-03 22:07:45 +0200

    add classes for css

--- resources/views/kochbuecher/create.blade.php
+++ resources/views/kochbuecher/create.blade.php
@@ -6,8 +6,8 @@
         @csrf
         <label for="kName">Name des Kochbuchs:</label>
         <input id="kName" name="kName" type="text" minlength="2" required><br>
-        <input type="reset" value="Abbrechen"> <!-- reset = Formulardaten werden gelöscht-->
-        <input type="submit" value="Kochbuch erstellen">
+        <input type="reset" class="abortbtn" value="Abbrechen"> <!-- reset = Formulardaten werden gelöscht-->
+        <input type="submit" class="finishbtn" value="Kochbuch erstellen">
     </form>
     <!-- Quelle https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit-->
 @endsection
