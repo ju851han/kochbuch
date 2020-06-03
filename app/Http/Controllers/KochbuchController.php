2759ec892155ab6f706eee39735af24a9e63659e
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-03 22:04:48 +0200

    correct comments

--- app/Http/Controllers/KochbuchController.php
+++ app/Http/Controllers/KochbuchController.php
@@ -38,6 +38,7 @@ class KochbuchController extends Controller
     }
 
     /**
+     * Shows one Kochbuch
      * @param $kid
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
