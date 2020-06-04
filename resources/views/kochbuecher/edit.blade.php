f05084f49379e1e6ce0126bf10bdbf3010de8f99
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-04 08:22:37 +0200

    change content of h2 and add classes for buttons

--- resources/views/kochbuecher/edit.blade.php
+++ resources/views/kochbuecher/edit.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>Kochbuch erstellen</h2>
+    <h2>Kochbuch bearbeiten</h2>
     <form method="post" action="/kochbuecher/{{$k->kID}}/update">
         @csrf
         <label for="kName">Name des Kochbuchs:</label>
         <input id="kName" name="kName" type="text" minlength="2" required value="{{$k->kName}}"><br>
-        <input type="reset" value="Abbrechen"> <!-- reset = Formulardaten werden gelöscht-->
-        <input type="submit" value="Bearbeiten">
+        <input type="reset" class="abortbtn" value="Abbrechen"> <!-- reset = Formulardaten werden gelöscht-->
+        <input type="submit" class="normalbtn" value="Bearbeiten">
     </form>
     <!-- Quelle https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit-->
 @endsection
