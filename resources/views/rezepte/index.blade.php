b8786bc9b35e2faf0dc5686a305b23e279fe6c53
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-11 13:30:26 +0200

    add buttons, delete kategorie for a better view

--- resources/views/rezepte/index.blade.php
+++ resources/views/rezepte/index.blade.php
@@ -6,18 +6,25 @@
         <thead class="thead_orangered">
         <tr>
             <th>Rezept Name</th>
-            <th>Kategorien</th>
             <th>Zeit</th>
             <th>Kosten</th>
+            <th></th>
         </tr>
         </thead>
         <tbody class="background2ndTR">
         @foreach ($rezepte as $r)
             <tr>
                 <td> {{  $r->rName }} </td>
-                <td> {{  $r->kategorie }}</td>
-                <td> {{  $r->zeit }}</td>
-                <td> {{  $r->kostenjePortion }}</td>
+                <td> {{  $r->zeit }}</td> {{--TODO Zeit richtig angeben--}}
+                <td> {{  $r->kostenjePortion }} € </td>
+                <td>{{--TODO Buttons formatting--}}
+                    <button class="normalbtn" onclick="window.location.href='/rezepte/{{$r->rID}}'"
+                            title="Rezept ansehen"><i class='fas fa-eye btn_i'></i></button>
+                    <button class="normalbtn" onclick="window.location.href='/rezepte/{{$r->rID}}/edit'"
+                            title="Rezept bearbeiten"><i class="material-icons btn_i">edit</i></button>
+                    <button class="normalbtn" onclick="window.location.href='/rezepte/{{$r->rID}}/destroy'"
+                            title="Rezept löschen"><i class="fas fa-trash-alt btn_i"></i></button>
+                </td>
             </tr>
         @endforeach
         </tbody>
