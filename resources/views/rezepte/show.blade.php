4909b079eeef35a62f1d757c1fdac83de97e0638
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-11 18:12:28 +0200

    add button

--- resources/views/rezepte/show.blade.php
+++ resources/views/rezepte/show.blade.php
@@ -23,5 +23,6 @@
             <td></td>
         </tr>
     </table>
-
+    <button class="normalbtn" onclick="window.location.href='/rezepte/{{$r->rID}}/edit'"
+            title="Rezept bearbeiten"><i class="material-icons btn_i">edit</i> Rezept bearbeiten</button>
 @endsection
