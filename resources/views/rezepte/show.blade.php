92cbad01b2a2d0875e8603318be4b963eb4a822f
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-11 18:07:04 +0200

    change layoutting

--- resources/views/rezepte/show.blade.php
+++ resources/views/rezepte/show.blade.php
@@ -1,24 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Rezept: XY</h1>
+    <h2>Rezept: {{  $r->rName }} </h2>
+    <p>Kategorien: {{  $r->kategorie }} | <i class="far fa-clock"></i> Zeit : {{  $r->zeit }} min</p>
     <!-- https://mdbootstrap.com/docs/jquery/forms/search/ -->
-    <label for="portion">Portion(en):</label><input id="portion" name="portion" type="number" step="0.5" min="0" max="50">
+    <label for="portion">Portion(en):</label><input id="portion" name="portion" type="number" step="0.5" min="0"
+                                                    max="50" value="1">
+    <?php
+    echo "<p> Kosten: " . $r->kostenjePortion * 2 . " €" . "</p>"
+    ?> {{--TODO Kosten *Portionen--}}
+
+
+    {{--TODO Zutaten einfügen--}}
+    <h3>Zutaten</h3>
     <table class="table">
-        <thead class="thead_orangered">
         <tr>
-            <th>Rezept Name</th>
-            <th>Kategorien</th>
-            <th>Zeit</th>
-            <th>Kosten</th>
+            <th>Menge</th>
+            <th>Zutat</th>
         </tr>
-        </thead>
-        <tbody class="background2ndTR">
-            <tr>
-                <td> {{  $r->rName }} </td>
-                <td> {{  $r->kategorie }}</td>
-                <td> {{  $r->zeit }}</td>
-                <td> {{  $r->kostenjePortion }}</td>
-            </tr>
+        <tr>
+            <td></td>{{--Menge * Portion--}}
+            <td></td>
+        </tr>
     </table>
+
 @endsection
