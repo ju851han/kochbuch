0d456257c0234f2adcaf382398485d9e0e889d66
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-11 13:16:35 +0200

    add button show kochbuch

--- resources/views/kochbuecher/index.blade.php
+++ resources/views/kochbuecher/index.blade.php
@@ -16,12 +16,20 @@
                 <tr>
                     <td> {{  $k->kID }} </td> {{--TODO i instead of kID -> Sonst sind lücken vorhanden --}}
                     <td> {{  $k->kName }}</td>
-                    <td> <button class="normalbtn" onclick="window.location.href='/kochbuecher/{{$k->kID}}/edit'"  title="Kochbuch bearbeiten"><i class="material-icons btn_i">edit</i></button>
-                        <button class="normalbtn" onclick="window.location.href='/kochbuecher/{{$k->kID}}/destroy'"  title="Kochbuch löschen"><i class="fas fa-trash-alt btn_i"></i></button></td>
+                    <td>
+                        <button class="normalbtn" onclick="window.location.href='/kochbuecher/{{$k->kID}}'"
+                                title="Kochbuch bearbeiten"><i class='fas fa-eye btn_i'></i></button>
+                        <button class="normalbtn" onclick="window.location.href='/kochbuecher/{{$k->kID}}/edit'"
+                                title="Kochbuch bearbeiten"><i class="material-icons btn_i">edit</i></button>
+                        <button class="normalbtn" onclick="window.location.href='/kochbuecher/{{$k->kID}}/destroy'"
+                                title="Kochbuch löschen"><i class="fas fa-trash-alt btn_i"></i></button>
+                    </td>
                 </tr>
             @endforeach
             </tbody>
         </table>
-        <button class="normalbtn" onclick="window.location.href='kochbuecher/create'" ><i class="material-icons btn_i">add_circle</i> Neues Kochbuch erstellen</button>
+        <button class="normalbtn" onclick="window.location.href='kochbuecher/create'"><i class="material-icons btn_i">add_circle</i>
+            Neues Kochbuch erstellen
+        </button>
     </section>
 @endsection
