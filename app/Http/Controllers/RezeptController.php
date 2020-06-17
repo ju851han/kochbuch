70adcd2b4175d8a1e221b5da1c9502eaeefe8ab1
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-17 20:52:38 +0200

    add methods

--- app/Http/Controllers/RezeptController.php
+++ app/Http/Controllers/RezeptController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Rezept;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class RezeptController extends Controller
 {
@@ -25,7 +26,8 @@ class RezeptController extends Controller
      */
     public function index()
     {
-        //
+        $rezepte = Rezept::all();
+        return view('rezepte/index')->with('rezepte', $rezepte);
     }
 
     /**
@@ -33,9 +35,11 @@ class RezeptController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        /*TODO authorized Role wo festzulegen?*/
+        /*     $request->user()->authorizeRole('logged_user');*/
+        return view('rezepte/create');
     }
 
     /**
@@ -46,41 +50,77 @@ class RezeptController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        /*TODO authorized Role wo festzulegen?*/
+        /*     $request->user()->authorizeRole('logged_user');*/
+        /*TODO Validation */
+        $rezept = new Rezept;
+        $rezept->rName= $request->rName;
+        $rezept->kategorie= $request->kategorie;
+        $rezept->zeit= $request->zeit;
+        $rezept->kostenjePortion= $request->kostenjePortion;
+        $rezept->zubereitung= $request->zubereitung;
+        $rezept->save();
+
+
+        return redirect()->action('RezeptController@index');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Rezept  $rezept
+     * @param $rID
      * @return \Illuminate\Http\Response
      */
-    public function show(Rezept $rezept)
+    public function show($rID)
     {
-        //
+        /*TODO authorized Role wo festzulegen?*/
+        /*     $request->user()->authorizeRole('logged_user');*/
+        $rezept = Rezept::find($rID);
+        if(is_null($rezept)){
+            return redirect()->action('RezeptController@index');
+        }
+        return view('rezepte/show')->with('r',$rezept);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Rezept  $rezept
+     * @param Request $request
+     * @param $rID
      * @return \Illuminate\Http\Response
      */
-    public function edit(Rezept $rezept)
+    public function edit(Request $request,$rID)
     {
-        //
+        /*TODO authorized Role wo festzulegen?*/
+        /*     $request->user()->authorizeRole('logged_user');*/
+        $rezept = Rezept::find($rID);
+        if(is_null($rezept)){
+            return redirect()->action('RezeptController@index');
+        }
+        return view('rezepte/edit')->with('r',$rezept);
+
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Rezept  $rezept
+     * @param  \Illuminate\Http\Request $request
+     * @param $rID
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Rezept $rezept)
+    public function update(Request $request, $rID)
     {
-        //
+        /*TODO authorized Role wo festzulegen?*/
+        /*     $request->user()->authorizeRole('logged_user');*/
+        $rezept=Rezept::find($rID);
+        /*TODO Validation */
+        $rezept->rName= $request->rName;
+        $rezept->kategorie= $request->kategorie;
+        $rezept->zeit= $request->zeit;
+        $rezept->kostenjePortion= $request->kostenjePortion;
+        $rezept->zubereitung= $request->zubereitung;
+        $rezept->save();
+        return redirect()->action('RezeptController@show',['rID'=>$rID]);
     }
 
     /**
@@ -89,8 +129,14 @@ class RezeptController extends Controller
      * @param  \App\Rezept  $rezept
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Rezept $rezept)
+    public function destroy(Request $request,$rID)
     {
-        //
+        /*TODO authorized Role wo festzulegen?*/
+        /*     $request->user()->authorizeRole('admin');*/
+        $rezept = Rezept::find($rID);
+        $rezept->delete();
+        Session::flash('alert-success', 'Rezept '.$rezept->rName.' wurde erfolgreich gelöscht.');
+        return redirect()->action('RezeptController@index');
+
     }
 }
