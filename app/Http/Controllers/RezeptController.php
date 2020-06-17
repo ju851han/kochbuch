58f6638909853867b3e86a4153ca78721746c568
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-17 17:57:57 +0200

    add construct function

--- app/Http/Controllers/RezeptController.php
+++ app/Http/Controllers/RezeptController.php
@@ -8,6 +8,17 @@ use Illuminate\Http\Request;
 class RezeptController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
--- app/Http/Controllers/ZutatController.php
+++ app/Http/Controllers/ZutatController.php
@@ -7,12 +7,23 @@ use Illuminate\Http\Request;
 
 class ZutatController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request,$kid)
     {
         //
     }
