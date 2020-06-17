1e9cb6f9ea19b8ea0f1bb8b241c4eccba242f41a
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-17 12:03:39 +0200

    add bootstrap grid classes <div class="row"> &<section class="col-xs-auto col-md-8 offset-md-2">

--- resources/views/rezepte/index.blade.php
+++ resources/views/rezepte/index.blade.php
@@ -2,33 +2,35 @@
 
 @section('content')
     <div class="container">
-        <section>
-            <h2>Rezepte Übersicht</h2>
-            <table class="table">
-                <thead class="thead_orangered">
-                <tr>
-                    <th>Rezept Name</th>
-                    <th>Zeit</th>
-                    <th>Kosten</th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody class="background2ndTR">
-                @foreach ($rezepte as $r)
+        <div class="row">
+            <section class="col-xs-auto col-md-8 offset-md-2">
+                <h2>Rezepte Übersicht</h2>
+                <table class="table">
+                    <thead class="thead_orangered">
                     <tr>
-                        <td> {{  $r->rName }} </td>
-                        <td> {{  $r->zeit }}</td> {{--TODO Zeit richtig angeben--}}
-                        <td> {{  $r->kostenjePortion }} €</td>
-                        <td>
-                            <button class="normalbtn" onclick="window.location.href='/rezepte/{{$r->rID}}'"
-                                    title="Rezept ansehen"><i class='fas fa-eye btn_i'></i></button>
-                            <button class="normalbtn" onclick="window.location.href='/rezepte/{{$r->rID}}/edit'"
-                                    title="Rezept bearbeiten"><i class="material-icons btn_i">edit</i></button>
-                        </td>
+                        <th>Rezept Name</th>
+                        <th>Zeit</th>
+                        <th>Kosten</th>
+                        <th></th>
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </section>
+                    </thead>
+                    <tbody class="background2ndTR">
+                    @foreach ($rezepte as $r)
+                        <tr>
+                            <td> {{  $r->rName }} </td>
+                            <td> {{  $r->zeit }}</td> {{--TODO Zeit richtig angeben--}}
+                            <td> {{  $r->kostenjePortion }} €</td>
+                            <td>
+                                <button class="normalbtn" onclick="window.location.href='/rezepte/{{$r->rID}}'"
+                                        title="Rezept ansehen"><i class='fas fa-eye btn_i'></i></button>
+                                <button class="normalbtn" onclick="window.location.href='/rezepte/{{$r->rID}}/edit'"
+                                        title="Rezept bearbeiten"><i class="material-icons btn_i">edit</i></button>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </section>
+        </div>
     </div>
 @endsection
--- resources/views/rezepte/show.blade.php
+++ resources/views/rezepte/show.blade.php
@@ -2,32 +2,35 @@
 
 @section('content')
     <div class="container">
-        <section>
-            <h2>Rezept: {{  $r->rName }} </h2>
-            <p>Kategorien: {{  $r->kategorie }} | <i class="far fa-clock"></i> Zeit : {{  $r->zeit }} min</p>
-            <!-- https://mdbootstrap.com/docs/jquery/forms/search/ -->
-            <label for="portion">Portion(en):</label><input id="portion" name="portion" type="number" step="0.5" min="0"
-                                                            max="50" value="1">
-            <?php
-            echo "<p> Kosten: " . $r->kostenjePortion * 2 . " €" . "</p>"
-            ?> {{--TODO Kosten *Portionen--}}
+        <div class="row">
+            <section class="col-xs-auto col-md-8 offset-md-2">
+                <h2>Rezept: {{  $r->rName }} </h2>
+                <p>Kategorien: {{  $r->kategorie }} | <i class="far fa-clock"></i> Zeit : {{  $r->zeit }} min</p>
+                <!-- https://mdbootstrap.com/docs/jquery/forms/search/ -->
+                <label for="portion">Portion(en):</label><input id="portion" name="portion" type="number" step="0.5"
+                                                                min="0"
+                                                                max="50" value="1">
+                <?php
+                echo "<p> Kosten: " . $r->kostenjePortion * 2 . " €" . "</p>"
+                ?> {{--TODO Kosten *Portionen--}}
 
 
-            {{--TODO Zutaten einfügen--}}
-            <h3>Zutaten</h3>
-            <table class="table">
-                <tr>
-                    <th>Menge</th>
-                    <th>Zutat</th>
-                </tr>
-                <tr>
-                    <td></td>{{--Menge * Portion--}}
-                    <td></td>
-                </tr>
-            </table>
-            <button class="normalbtn" onclick="window.location.href='/rezepte/{{$r->rID}}/edit'"
-                    title="Rezept bearbeiten"><i class="material-icons btn_i">edit</i> Rezept bearbeiten
-            </button>
-        </section>
+                {{--TODO Zutaten einfügen--}}
+                <h3>Zutaten</h3>
+                <table class="table">
+                    <tr>
+                        <th>Menge</th>
+                        <th>Zutat</th>
+                    </tr>
+                    <tr>
+                        <td></td>{{--Menge * Portion--}}
+                        <td></td>
+                    </tr>
+                </table>
+                <button class="normalbtn" onclick="window.location.href='/rezepte/{{$r->rID}}/edit'"
+                        title="Rezept bearbeiten"><i class="material-icons btn_i">edit</i> Rezept bearbeiten
+                </button>
+            </section>
+        </div>
     </div>
 @endsection
