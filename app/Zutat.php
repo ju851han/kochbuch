a2387ecfb6974df5c77544c986e06834eb6adb95
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-18 17:19:43 +0200

    casts primary key to string so that zutatcontroller@index can read the right value of zName

--- app/Zutat.php
+++ app/Zutat.php
@@ -8,6 +8,8 @@ class Zutat extends Model
 {
     protected $table =  "zutats";
     protected $fillable = ['zName','kostenjeEinheit','mengeneinheit','produktgruppe'];
+    protected $casts =['zName' =>'string'];
+    /*https://stackoverflow.com/questions/38463624/laravel-eloquent-model-id-as-string-return-wrong-value-in*/
     protected $primaryKey='zName';
 
 }
