bbd380a1d5b53380862c8dd2215a39c4bbd601d7
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-18 10:00:52 +0200

    add role columns to users table

---
+++ database/migrations/2020_06_18_075800_add_roles_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class AddRolesToUsersTable generated with cmd: php artisan make:migration add_roles_to_users_table --table=users
+ */
+class AddRolesToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role',20)->default('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
