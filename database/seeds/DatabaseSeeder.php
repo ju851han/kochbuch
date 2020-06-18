66337dc327ae7a8f10b42b785a382c05b1c7cd3b
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-18 10:00:07 +0200

    add calling instructions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(KochbuchesTableSeeder::class);
+        $this->call(RezeptsTableSeeder::class);
+        $this->call(ZutatsTableSeeder::class);
     }
 }
