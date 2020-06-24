accf23c1a8d43e6342e529ef23483505fea8b3d2
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-24 22:14:48 +0200

    add user_id column + value

--- database/seeds/KochbuchesTableSeeder.php
+++ database/seeds/KochbuchesTableSeeder.php
@@ -12,8 +12,8 @@ class KochbuchesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::insert("INSERT INTO kochbuches (kName, created_at, updated_at) VALUES ('Erstes Kochbuch',NOW(),NOW());");
-        DB::insert("INSERT INTO kochbuches (kName, created_at, updated_at) VALUES ('Lieblingsrezepte',NOW(),NOW());");
+        DB::insert("INSERT INTO kochbuches (kName, users_id, created_at, updated_at) VALUES ('Erstes Kochbuch',1,NOW(),NOW());");
+        DB::insert("INSERT INTO kochbuches (kName, users_id, created_at, updated_at) VALUES ('Lieblingsrezepte',2, NOW(),NOW());");
 
     }
 }
