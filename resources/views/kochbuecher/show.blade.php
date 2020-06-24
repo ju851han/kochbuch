66beb20c3051f04369b9efc6c763c98d03d34570
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-24 16:13:20 +0200

    view can show the related rezepte in kochbuch

--- resources/views/kochbuecher/show.blade.php
+++ resources/views/kochbuecher/show.blade.php
@@ -5,9 +5,9 @@
         <div class="row">
             <section class="col-xs-auto col-md-8 offset-md-2">
                 <h3><i class="fa fa-book"></i> Kochbuch:</h3>
-                <h1 id="special">{{ $k->kName }}</h1>
-                <p class="untertitel">erstellt am: {{$k->erstelltam}}; zuletzt aktualisert
-                    am: {{$k->aktualisiertam}}</p>
+                <h1 id="special">{{ $kochbuch->kName }}</h1>
+                <p class="untertitel">erstellt am: {{$kochbuch->erstelltam}}; zuletzt aktualisert
+                    am: {{$kochbuch->aktualisiertam}}</p>
                 <h2>Inhaltsverzeichnis</h2>
                 <table class="table">
                     <thead>
@@ -16,29 +16,26 @@
                         <th>Kategorien</th>
                         <th>Zeit</th>
                         <th>Kosten</th>
+                        <th></th>
                     </tr>
                     </thead>
                     <tbody class="background2ndTR">
-                    <tr> {{--TODO insert Rezeptdata --}}
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                    <tr>
+                        @foreach($kochbuch->rezepts  as $rezept )
+                        <td>{{ $rezept->rName}}</td>
+                        <td>{{  $rezept->kategorie }}</td>
+                        <td>{{  $rezept->zeit }} min</td>
+                        <td>{{  $rezept->kostenjePortion }} €</td>
+                            <td><button class="normalbtn btn" onclick="window.location.href='/rezepte/{{$rezept->rID}}'"
+                                        title="Rezept ansehen"><i class='fas fa-eye btn_i'></i></button></td>
+                        @endforeach
                     </tr>
-                    {{--@foreach ($rezepte as $r)
-                        <tr>
-                            <td> {{  $r->rName }} </td>
-                            <td> {{  $r->kategorie }}</td>
-                            <td> {{  $r->zeit }}</td>
-                            <td> {{  $r->kostenjePortion }}</td>
-                        </tr>
-                    @endforeach--}}
                     </tbody>
                 </table>
-                <button class="normalbtn btn" onclick="window.location.href='/kochbuecher/{{$k->kID}}/edit'"
+                <button class="normalbtn btn" onclick="window.location.href='/kochbuecher/{{$kochbuch->kID}}/edit'"
                         title="Kochbuch bearbeiten"><i class="material-icons btn_i">edit</i> Bearbeiten
                 </button>
-                <button class="normalbtn btn" onclick="window.location.href='/kochbuecher/{{$k->kID}}/destroy'"
+                <button class="normalbtn btn" onclick="window.location.href='/kochbuecher/{{$kochbuch->kID}}/destroy'"
                         title="Kochbuch löschen"><i class="fas fa-trash-alt btn_i"></i> Löschen
                 </button>
             </section>
