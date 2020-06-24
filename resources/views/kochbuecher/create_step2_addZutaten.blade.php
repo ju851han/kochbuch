e1225c3324b1e4d69b415ae81b4acb56752a58c1
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-24 10:38:02 +0200

    add form for zutaten

--- resources/views/kochbuecher/create_step2_addZutaten.blade.php
+++ resources/views/kochbuecher/create_step2_addZutaten.blade.php
@@ -7,13 +7,55 @@
                 <h2>Kochbuch erstellen</h2>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item active" aria-current="page">1. Name eingeben</li>
+                        <li class="breadcrumb-item active" aria-current="page">1. Name des Kochbuches eingeben</li>
                         <li class="breadcrumb-item active" aria-current="page">2. Zutaten eingeben</li>
                     </ol>
                 </nav>
+                </nav>
                 <form method="post" action="/kochbuecher/create_step4">
                     @csrf
-
+                    <div class="form-group">
+                        <label for="zName">Zutat</label>
+                        <input class="form-control" id="zName" name="zName" type="text" minlength="2" maxlength="256"
+                               required>
+                    </div>
+                    <br>
+                    <!-- TODO <label for="Menge">Menge</label>
+                    <input id="Menge" name="Menge" type="number" min="0.5" max="1000000" required></br></br>-->
+                    <div class="form-group">
+                        <label for="mengeneinheit">Mengeneinheit</label>
+                        <input class="form-control" id="mengeneinheit" name="mengeneinheit" type="text" minlength="1"
+                               maxlength="125" required>
+                    </div>
+                    <br>
+                    <div class="form-group">
+               <label for="kostenJeEinheit">Kosten je Einheit</label><div class="form-row">
+                            <input class="form-control col-md-10 order-1" id="kostenJeEinheit" name="kostenJeEinheit"
+                                   type="number" min="0" step="0.01" required>
+                            <label class="col-md-2 order-2">  €</label>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label for="produktgruppe">Produktgruppe</label>
+                        <input class="form-control" list="produktgruppen" id="produktgruppe" name="produktgruppe"
+                               type="text">
+                        <datalist id="produktgruppen">
+                            <option value="Backwaren"></option>
+                            <option value="Fisch&Meeresfrüchte"></option>
+                            <option value="Fleisch"></option>
+                            <option value="Getränke"></option>
+                            <option value="Grundnahrungsmittel"></option>
+                            <option value="Konserven"></option>
+                            <option value="Milchprodukte"></option>
+                            <option value="Obst&Gemüse"></option>
+                            <option value="Naschsachen&Knabbergebäck"></option>
+                            <option value="Tiefkühl"></option>
+                        </datalist>
+                    </div>
+                    <br>
+                    <input type="reset" class="abortbtn btn" value="Abbrechen">
+                    <input type="submit" class="normalbtn btn" value="Zutat hinzufügen">
                 </form>
             </section>
         </div>
