7df22e13a4ccbf44f9e6be62fa7f5c2602a57ad2
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-24 16:22:02 +0200

    view shows the zutaten in rezept

--- resources/views/rezepte/show.blade.php
+++ resources/views/rezepte/show.blade.php
@@ -4,18 +4,17 @@
     <div class="container">
         <div class="row">
             <section class="col-xs-auto col-md-8 offset-md-2">
-                <h2>Rezept: {{  $r->rName }} </h2>
-                <p>Kategorien: {{  $r->kategorie }} | <i class="far fa-clock"></i> Zeit : {{  $r->zeit }} min</p>
+                <h2>Rezept: {{  $rezept->rName }} </h2>
+                <p>Kategorien: {{  $rezept->kategorie }} | <i class="far fa-clock"></i> Zeit : {{  $rezept->zeit }} min
+                </p>
                 <!-- https://mdbootstrap.com/docs/jquery/forms/search/ -->
                 <label for="portion">Portion(en):</label><input id="portion" name="portion" type="number" step="0.5"
                                                                 min="0"
                                                                 max="50" value="1">
                 <?php
-                echo "<p> Kosten: " . $r->kostenjePortion * 2 . " €" . "</p>"
+                echo "<p> Kosten: " . $rezept->kostenjePortion * 2 . " €" . "</p>"
                 ?> {{--TODO Kosten *Portionen--}}
 
-
-                {{--TODO Zutaten einfügen--}}
                 <h3>Zutaten</h3>
                 <table class="table">
                     <tr>
@@ -23,14 +22,16 @@
                         <th>Zutat</th>
                     </tr>
                     <tr>
-                        <td></td>{{--Menge * Portion--}}
-                        <td></td>
+                        @foreach($rezept->zutats  as $zutat )
+                            <td></td>{{--Menge * Portion--}}
+                            <td>{{ $zutat->zName }}</td>
+                        @endforeach
                     </tr>
                 </table>
 
                 <h3>Zubereitung</h3>
-                <p>{{$r->zubereitung}}</p>
-                <button class="normalbtn btn" onclick="window.location.href='/rezepte/{{$r->rID}}/edit'"
+                <p>{{$rezept->zubereitung}}</p>
+                <button class="normalbtn btn" onclick="window.location.href='/rezepte/{{$rezept->rID}}/edit'"
                         title="Rezept bearbeiten"><i class="material-icons btn_i">edit</i> Rezept bearbeiten
                 </button>
             </section>
