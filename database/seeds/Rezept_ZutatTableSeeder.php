398b430ca93a86cbc756f9d4f6196e44cf615b1e
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-25 14:01:00 +0200

    correction rezeptID

--- database/seeds/Rezept_ZutatTableSeeder.php
+++ database/seeds/Rezept_ZutatTableSeeder.php
@@ -11,12 +11,12 @@ class Rezept_ZutatTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('1','1','Butter','100');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('2','1','Zucker','100');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('3','1','Vanillezucker','2');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('4','1','Weichweizengrieß','80');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('5','1','Pfirsich','125');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('6','1','Ei(er)','4');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('1','2','Butter','100');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('2','2','Zucker','100');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('3','2','Vanillezucker','2');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('4','2','Weichweizengrieß','80');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('5','2','Pfirsich','125');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('6','2','Ei(er)','4');");
         /*        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('1','','','');");*/
     }
 }
