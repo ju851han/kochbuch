ee11ca0d188c7409beef80c8df54bc5ef9b6822c
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-26 15:47:16 +0200

    add Impressum

---
+++ resources/views/impressum.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app_unlogged')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <section class="col-xs-auto col-md-8 offset-md-2">
+                <h2>Impressum</h2>
+               {{--TODO add text--}}
+            </section>
+        </div>
+    </div>
+    <!-- Quelle https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit-->
+@endsection
