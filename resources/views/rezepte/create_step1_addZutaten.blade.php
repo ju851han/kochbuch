ca3117c2080116d1dd78eb8a3d3ae072f065ee37
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-26 21:01:29 +0200

    change layouting; add jquery for adding mengeneinheit every time when the zName is changing

--- resources/views/rezepte/create_step1_addZutaten.blade.php
+++ resources/views/rezepte/create_step1_addZutaten.blade.php
@@ -12,51 +12,28 @@
                 </nav>
                 <form method="post" action="/rezepte/create_step2_Rezept">
                     @csrf
-                    <div class="form-group">
-                        <label for="zName">Zutat</label>
-                        <input class="form-control" id="zName" name="zName" type="text" minlength="2" maxlength="256"
-                               required>
-                    </div>
-                    <br>
-                    <div class="form-group">
-                        <label for="menge">Menge</label>
-                        <input id="menge" class="form-control" name="menge" type="number" min="0.5" max="1000000"
-                               step="0.5" required>
-                    </div>
-                    </br>
-                    <div class="form-group">
-                        <label for="mengeneinheit">Mengeneinheit</label>
-                        <input class="form-control" id="mengeneinheit" name="mengeneinheit" type="text" minlength="1"
-                               maxlength="125" required>
-                    </div>
-                    <br>
-                    <div class="form-group">
-
-                        <label for="kostenJeEinheit">Kosten je Einheit</label>
-                        <div class="form-row">
-                            <input class="form-control col-md-10 order-1" id="kostenJeEinheit" name="kostenJeEinheit"
-                                   type="number" min="0" step="0.01" required>
-                            <label class="col-md-2 order-2"> €</label>
+                    <div class="form-row">
+                        <div class="form-group col-md-3">
+                            <label for="menge">Menge</label>
+                            <input id="menge" class="form-control" name="menge" type="number" min="0.5" max="1000000"
+                                   step="0.5" required>
+                        </div>
+                        <div class="form-group col-md-1">
+                            <label></label>
+                            <output id="mengeneinheit" name="mengeneinheit">Stk</output>
+                        </div>
+                        <div class="form-group col-md-5">
+                            <label for="zName">Zutat</label>
+                            <select name="zName_1" id="zName_1" class="form-control"
+                                    onchange="updateMengeneinheit(this.value);">
+                                @foreach ($zutaten as $z)
+                                    <option value="{{$z->zName}}">{{$z->zName}}</option>
+                                @endforeach
+                            </select>
                         </div>
                     </div>
-                    <br>
-
-                    <div class="form-group">
-                        <label for="produktgruppe">Produktgruppe</label>
-                        <input class="form-control" list="produktgruppen" id="produktgruppe" name="produktgruppe"
-                               type="text">
-                        <datalist id="produktgruppen">
-                            <option value="Backwaren"></option>
-                            <option value="Fisch&Meeresfrüchte"></option>
-                            <option value="Fleisch"></option>
-                            <option value="Getränke"></option>
-                            <option value="Grundnahrungsmittel"></option>
-                            <option value="Konserven"></option>
-                            <option value="Milchprodukte"></option>
-                            <option value="Obst&Gemüse"></option>
-                            <option value="Naschsachen&Knabbergebäck"></option>
-                        </datalist>
-                    </div>
+                    <input class="form-control col-md-10 order-1" id="kostenJeEinheit" name="kostenJeEinheit"
+                            required>
                     <br>
                     <input type="reset" class="abortbtn btn" value="Abbrechen">
                     <input type="submit" class="normalbtn btn" value="Weiter">
@@ -64,5 +41,14 @@
             </section>
         </div>
     </div>
-    <!-- Quelle https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit-->
+    <script>
+        function updateMengeneinheit(val) {
+            @foreach ($zutaten as $z)
+            if (val ==="{{$z->zName}}") {
+                $('#mengeneinheit').val("{{$z->mengeneinheit}}");
+                $('#mengeneinheit').text("{{$z->mengeneinheit}}");
+            }
+            @endforeach
+        }
+    </script>
 @endsection
