74443729756e67a08387657a15f3dcc2968eeacd
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-26 14:22:52 +0200

    edited show

--- resources/views/zutaten/show.blade.php
+++ resources/views/zutaten/show.blade.php
@@ -4,22 +4,20 @@
     <div class="container">
         <div class="row">
             <section class="col-12 col-md-8 offset-md-2">
-                <h2>Zutat</h2>
+                <h2>Zutat: {{  $z->zName }}</h2>
                 <table class="table">
                     <thead>
                     <tr>
-                        <th>Zutaten Name</th>
+                        <th>Produktgruppe</th>
                         <th>Kosten je Einheit</th>
                         <th>Mengeneinheit</th>
-                        <th>Produktgruppe</th>
                     </tr>
                     </thead>
                     <tbody class="background2ndTR">
                     <tr>
-                        <td> {{  $z->zName }} </td>
-                        <td> {{  $z->mengeneinheit }}</td>
-                        <td> {{  $z->kostenJeEinheit }}</td>
                         <td> {{  $z->produktgruppe }}</td>
+                        <td> {{  $z->kostenjeEinheit }} €</td>
+                        <td> {{  $z->mengeneinheit }}</td>
                     </tr>
                     </tbody>
                 </table>
