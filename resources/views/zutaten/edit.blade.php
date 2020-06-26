f3780739de23a5f2e205d5707e35cbf36fabe264
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-26 14:31:37 +0200

    edited beschreibung, edited kosten je einheit & produktgruppe

--- resources/views/zutaten/edit.blade.php
+++ resources/views/zutaten/edit.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <section class="col-12 col-md-8 offset-md-2">
                 <h2>Bearbeiten der Zutat</h2>
-                <p>In dieser Ansicht kannst du die Zutaten bearbeiten.</p>
+                <p class="beschreibung">In dieser Ansicht kannst du die Zutat bearbeiten.</p>
 
                 <form method="post" action="/zutaten/{{$z->zName}}/update">
                     @csrf
@@ -21,25 +21,19 @@
                                maxlength="125" required
                                value="{{$z->mengeneinheit}}">
                     </div>
-                    <br>
-                    <div class="form-group">
-                        <div class="row">
-                            <label for="kostenJeEinheit">Kosten je Einheit</label>
-                        </div>
-                        <div class="row">
-                            <input class="form-control col-10 col-md-7" id="kostenJeEinheit" name="kostenJeEinheit"
-                                   type="number"
+                    <div class="form-group"> {{--TODO funktioniert nicht--}}
+                        <label for="kostenJeEinheit">Kosten je Einheit</label>
+                        <div class="form-row">
+                            <input class="form-control text-right col-10 col-md-7" id="kostenJeEinheit" name="kostenJeEinheit" type="number"
                                    min="0"
-                                   step="0.01" placeholder="{{$z->kostenJeEinheit}}" value="{{$z->kostenJeEinheit}}"
-                                   required>
-                            <label class="col-2 col-md-1">€</label>
+                                   step="0.01"  value="{{$z->kostenjeEinheit}}" placeholder="{{$z->kostenjeEinheit}}" required>
+                            <p class="form-group col-2 col-md-1 waehrung"> €</p>
                         </div>
                     </div>
-                    <br>
                     <div class="form-group">
                         <label for="produktgruppe">Produktgruppe</label>
                         <input list="produktgruppen" class="form-control" id="produktgruppe" name="produktgruppe"
-                               type="text" value="{{$z->produktgruppe}}">
+                               type="text" placeholder="{{$z->produktgruppe}}" value="{{$z->produktgruppe}}">
                         <datalist id="produktgruppen">
                             <option value="Backwaren"></option>
                             <option value="Fisch&Meeresfrüchte"></option>
