ee1003a70ee0810c732a6986d449cc332afc3d38
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-26 15:45:38 +0200

    add form action & text

--- resources/views/kontakt.blade.php
+++ resources/views/kontakt.blade.php
@@ -5,17 +5,19 @@
         <div class="row">
             <section class="col-xs-auto col-md-8 offset-md-2">
                 <h2>Kontakt</h2>
-                <p>Gerne stehen wir Ihnen für Fragen jederzeit zur Verfügung.</p>
-                <!-- TODO Text hinzufügen-->
-                <form method="POST" action=""> <!--TODO action="fehlt"-->
+                <p>Haben Sie Fragen oder wollen Sie ein Feedback zur Web-Applikation "Kochplan" geben? <br>
+                   Dann füllen Sie einfach das folgende Formularfelder aus:</p>
+                {{--TODO https://html.form.guide/email-form/html-email-form/--}}
+                <form method="POST" action="mailto:julia.hansi@htwg-konstanz.de" enctype="text/plain">
                     <div class="form-group">
                         <label for="eMail">Ihre E-Mail Adresse:</label>
                         <input class="form-control" id="eMail" name="eMail" type="email" required></div>
                     <div class="form-group">
                         <label for="betreff">Betreff:</label>
-                        <input class="form-control" id="betreff" name="betreff" type="text" size="25"/></div>
+                        <input class="form-control" id="betreff" name="betreff" type="text" size="25" required></div>
                     <div class="form-group"><label for="text">Text:</label>
                         <textarea class="form-control" id="text" name="text" rows="5" cols="30"></textarea></div>
+                    <br>
                     <input type="submit" class="normalbtn btn" value="Senden">
                     <input type="reset" class="abortbtn btn" value="Abbrechen">
                 </form>
