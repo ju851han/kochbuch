2ac15408ab55efd14eb5140070ae648269f09237
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-29 10:42:04 +0200

    update form & jquery functions

--- resources/views/kochbuecher/create_step2b_2_createRezept.blade.php
+++ resources/views/kochbuecher/create_step2b_2_createRezept.blade.php
@@ -12,13 +12,12 @@
                         <li class="breadcrumb-item active" aria-current="page">3. Rezepte hinzufügen</li>
                     </ol>
                 </nav>
-                <form method="post" action="/kochbuecher/create_step4_overview">
+                <form method="post" action="/kochbuecher/create_step3">
                     @csrf
                     <div class="form-group">
                         <label for="rName">Name des Rezepts:</label>
                         <input class="form-control" id="rName" name="rName" type="text" required>
                     </div>
-                    <br>
                     <div class="form-group">
                         <label for="zubereitung">Beschreibe bitte die Zubereitungschritte des Gerichts:</label><br>
                         <textarea class="form-control" id="zubereitung" name="zubereitung" minlength="20"
@@ -26,27 +25,22 @@
                                   cols="50" rows="5"
                                   required></textarea><br>
                     </div>
-                    <br>
-                    <div class="form-group" onchange="kategory()">
+                    <div class="form-group" onchange="kategory();">
                         <label for="kategorie">Wähle die Kategorien aus, zu denen das Rezept passt:</label>
                         <br>
                         <div class="form-check">
                             <input class="form-check-input" id="Pasta" name="Pasta" type="checkbox" value="Pasta">
                             <label class="form-check-label" for="Pasta">Pasta</label></div>
-                        <br>
                         <div class="form-check">
                             <input class="form-check-input" id="Fleisch" name="Fleisch" type="checkbox"
                                    value="Fleisch">
                             <label class="form-check-label" for="Fleisch">Fleisch</label></div>
-                        <br>
                         <div class="form-check">
                             <input class="form-check-input" id="Fisch" name="Fisch" type="checkbox" value="Fisch">
                             <label class="form-check-label" for="Fisch">Fisch</label></div>
-                        <br>
                         <div class="form-check">
                             <input class="form-check-input" id="Snacks" name="Snacks" type="checkbox" value="Snacks">
                             <label class="form-check-label" for="Snacks">Snacks</label></div>
-                        <br>
                         <div class="form-check">
                             <input class="form-check-input" id="Vegetarisch" name="Vegetarisch" type="checkbox"
                                    value="Vegetarisch">
@@ -54,7 +48,6 @@
                         {{--https://stackoverflow.com/questions/16167675/return-the-value-of-a-js-function-and-use-it-as-the-value-for-an-input-button--}}
                         <input type="hidden" id="kategorie" name="kategorie" value="">
                     </div>
-                    <br>
                     <div class="form-group">
                         <label for="zeit">Bitte gebe die ungefähre Gesamtzeit in Minuten an, wie lange dein Gericht
                             benötigt?</label>
@@ -62,7 +55,7 @@
                         <input class="form-control-range" id="zeit" name="zeit" list="zeiten" type="range" min="15"
                                max="240"
                                step="15"
-                               onchange="updateTextInput(this.value);" required>
+                               onchange="updateZeitOutput(this.value);" required>
                         {{--https://stackoverflow.com/questions/10004723/html5-input-type-range-show-range-value--}}
                         {{--https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_rangeslider--}}
                         {{--https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range--}}
@@ -86,49 +79,44 @@
                             <option value="240" label="240 min"></option>
                         </datalist>
                         <br>
-                        <output id="textInput" for="zeit" type="text">135</output>
+                        <output id="zeit_output" for="zeit" type="text">135</output>
                         <span> min</span>
                     </div>
                     <br>
-                    <!-- TODO muss ausgerechnet werden und automatisch weitergegeben werden (input type=hidden) -->
-                    <div class="row form-group">
-                        <label for="kostenjePortion">Kosten je Portion:</label>
-                        <input class="form-control col-md-7 order-1" id="kostenjePortion"
-                               name="kostenjePortion" type="number" step="0.01"
-                               required>
-                        <label class="col-md-1 order-2">€</label>
+                    <div class="row justify-content-end">
+                        <div>
+                            <input type="reset" class="abortbtn btn" onclick="window.location.href='/rezepte/index'"
+                                   value="Abbrechen">
+                            <input type="submit" class="normalbtn btn" value="Weiter">
+                        </div>
                     </div>
-                    <br>
-                    <input type="reset" class="abortbtn btn" value="Abbrechen">
-                    <!-- reset = Formulardaten werden gelöscht-->
-                    <input type="submit" class="normalbtn btn" value="Weiter">
                 </form>
             </section>
         </div>
     </div>
-    <!-- Quelle https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit-->
     <script>
-        function updateTextInput(val) {
-            document.getElementById('textInput').value = val;
+        function updateZeitOutput(val) {
+            $('#zeit_output').val(val);
         }
+
         function kategory() {
             var txt = "";
-            if (document.getElementById('Pasta').checked) {
+            if ($('#Pasta').prop('checked') == true) {
                 txt = txt + " Pasta";
             }
-            if (document.getElementById('Fisch').checked) {
+            if ($('#Fisch').prop('checked') == true) {
                 txt = txt + " Fisch";
             }
-            if (document.getElementById('Fleisch').checked) {
+            if ($('#Fleisch').prop('checked') == true) {
                 txt = txt + " Fleisch";
             }
-            if (document.getElementById('Snacks').checked) {
+            if ($('#Snacks').prop('checked') == true) {
                 txt = txt + " Snacks";
             }
-            if (document.getElementById('Vegetarisch').checked) {
+            if ($('#Vegetarisch').prop('checked') == true) {
                 txt = txt + " Vegetarisch";
             }
-            document.getElementById('kategorie').value = txt;
+            $('#kategorie').val(txt);
         }
 
     </script>
