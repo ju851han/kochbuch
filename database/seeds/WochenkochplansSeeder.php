85529c7c01e5ea344dcd277cf21ff6ba98425520
Julia Hansi <ju851han@htwg-konstanz.de>
2020-06-30 21:11:41 +0200

    correction id

--- database/seeds/WochenkochplansSeeder.php
+++ database/seeds/WochenkochplansSeeder.php
@@ -19,13 +19,13 @@ class WochenkochplansSeeder extends Seeder
         DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('6','1','Samstag','1');");
         DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('7','1','Sonntag','1');");
 
-        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('1','2','Montag','1.5');");
-        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('2','2','Dienstag','1.5');");
-        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('3','2','Mittwoch','1.3');");
-        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('4','2','Donnerstag','1.5');");
-        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('5','2','Freitag','1.5');");
-        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('6','2','Samstag','1.5');");
-        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('7','2','Sonntag','2');");
+        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('8','2','Montag','1.5');");
+        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('9','2','Dienstag','1.5');");
+        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('10','2','Mittwoch','1.3');");
+        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('11','2','Donnerstag','1.5');");
+        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('12','2','Freitag','1.5');");
+        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('13','2','Samstag','1.5');");
+        DB::insert("INSERT INTO wochenkochplans (id, users_id, wochentag, portion)VALUES ('14','2','Sonntag','2');");
 
     }
 }
