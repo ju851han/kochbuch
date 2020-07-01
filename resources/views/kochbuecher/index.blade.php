3e6a84385fb39b117a2a14e227fba85622e9e96b
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-01 13:16:13 +0200

    formatted

--- resources/views/kochbuecher/index.blade.php
+++ resources/views/kochbuecher/index.blade.php
@@ -6,7 +6,7 @@
             <section class="col-xs-auto col-md-8 offset-md-2">
                 <h2><i class="fa fa-book"></i> Kochbücher</h2>
                 <br>
-                <table class="table tbl_kochbuecher"> <!-- TODO Formatting -->
+                <table class="table tbl_kochbuecher">
                     <thead class="thead_orangered">
                     <tr>
                         <th>Nr.</th>
@@ -16,16 +16,15 @@
                     </thead>
                     <tbody class="background2ndTR">
                     <?php
-                            $i=1;
-                            foreach($kochbuecher as $k){
-                            echo "<tr><td>".$i."</td>";
-                            $i ++;
-                            echo"<td>".$k->kName."</td>";
-                            echo"<td><button class='normalbtn btn' onclick=\"window.location.href='/kochbuecher/".$k->kID."'\" title='Kochbuch ansehen'><i class='fas fa-eye btn_i'></i></button>";
-                            echo"<button class='normalbtn btn' onclick=\"window.location.href='/kochbuecher/".$k->kID."/edit'\" title='Kochbuch bearbeiten'><i class='material-icons btn_i'>edit</i></button>";
-                            echo"<button class='normalbtn btn' onclick=\"window.location.href='/kochbuecher/".$k->kID."/destroy'\" title='Kochbuch löschen'><i class='fas fa-trash-alt btn_i'></i></button></tr>";
-                            }
-
+                    $i = 1;
+                    foreach ($kochbuecher as $k) {
+                        echo "<tr><td>" . $i . "</td>";
+                        $i++;
+                        echo "<td>" . $k->kName . "</td>";
+                        echo "<td><button class='normalbtn btn' onclick=\"window.location.href='/kochbuecher/" . $k->kID . "'\" title='Kochbuch ansehen'><i class='fas fa-eye btn_i'></i></button>";
+                        echo "<button class='normalbtn btn' onclick=\"window.location.href='/kochbuecher/" . $k->kID . "/edit'\" title='Kochbuch bearbeiten'><i class='material-icons btn_i'>edit</i></button>";
+                        echo "<button class='normalbtn btn' onclick=\"window.location.href='/kochbuecher/" . $k->kID . "/destroy'\" title='Kochbuch löschen'><i class='fas fa-trash-alt btn_i'></i></button></tr>";
+                    }
                     ?>
                     </tbody>
                 </table>
