7719bd20ddb851b227ceb40d9966ae0b90eb9636
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-01 18:32:31 +0200

    new add form

--- resources/views/wochenkochplan/edit.blade.php
+++ resources/views/wochenkochplan/edit.blade.php
@@ -5,66 +5,49 @@
         <div class="row">
             <section class="col-xs-auto col-md-8 offset-md-2">
                 <h2>Wochenkochplan <i class="fas fa-calendar-week"></i></h2>
-                <table>
+                <table class="table">
                     <thead>
                     <tr>
                         <th>Wochentag</th>
                         <th>geplantes Rezept</th>
                         <th>Zeit</th>
-                        <th>Kosten</th>
-                        <th>Portion</th> <!--TODO Portion eingeben ->  Kosten berechnen lassen -->
+                         <th>Kosten</th>
+                         <th>Portion</th> {{--TODO Portion eingeben ->  Kosten berechnen lassen --}}
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>Montag</td>
-                        <td>
-                            <button>Rezept hinzufügen</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Dienstag</td>
-                        <td>
-                            <button>Rezept hinzufügen</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Mittwoch</td>
-                        <td>
-                            <button>Rezept hinzufügen</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Donnerstag</td>
-                        <td>
-                            <button>Rezept hinzufügen</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Freitag</td>
-                        <td>
-                            <button>Rezept hinzufügen</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Samstag</td>
-                        <td>
-                            <button>Rezept hinzufügen</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Sonntag</td>
-                        <td>
-                            <button>Rezept hinzufügen</button>
-                        </td>
-                    </tr>
+                    @foreach( $wochentag as $w)
+                        <tr>
+                            <td>{{$w->wochentag}}</td>
+                            @if (is_null($w->rezepts()->where('wochenkochplan_id',$w->id)->first()))
+                                <td colspan="4">
+                                    <?php echo"hi"?>
+                                    <button>Rezept hinzufügen</button>
+                                </td>
+                            @else
+                                <td>
+                                    {{$w->rezepts()->where('wochenkochplan_id',$w->id)->first()->rName}}
+                                </td>
+                                <td>
+                                    {{$w->rezepts()->where('wochenkochplan_id',$w->id)->first()->zeit}} min
+                                </td>
+                                <td>
+                                    {{$w->rezepts()->where('wochenkochplan_id',$w->id)->first()->kostenjePortion}} €
+                                </td>
+                                <td>
+
+                                </td>
+                            @endif
+                        </tr>
+                    @endforeach
                     </tbody>
                 </table>
                 <br>
-                <button>Einkaufsliste erstellen</button>
+                <button class="normalbtn btn">Einkaufsliste erstellen</button>
                 <br>
-                <button type="reset" value="Entleeren">Entleeren</button> <!-- reset = Formulardaten werden gelöscht-->
-                <button type="submit" value="Speichern">Speichern</button>
+                <br>
+                <button type="reset" class="normalbtn btn" value="Entleeren">Entleeren</button> <!-- reset = Formulardaten werden gelöscht-->
+                <button type="submit" class="normalbtn btn" value="Speichern">Speichern</button>
             </section>
         </div>
     </div>
