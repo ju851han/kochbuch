3b2583afa6ae94ec5633fa9854f5c34b02ac201c
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-01 13:10:08 +0200

    add btns for kochbuecher rezepte wochen kochplan einkaufsliste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,5 +19,30 @@
             </div>
         </div>
     </div>
+    {{--TODO links--}}
+    <div class="row text-center justify-content-center">
+        <div class="col-3 abortbtn" onclick="window.location.href='/kochbuecher'">
+            <br>
+            <i class="fa fa-book"></i>
+            <p>Kochbücher</p>
+        </div>
+        <div class="col-3 normalbtn" onclick="window.location.href='/rezepte'">
+            <br>
+            {{--TODO insert rezept icon--}}
+            <p>Rezepte</p>
+        </div>
+    </div>
+    <div class="row justify-content-center text-center">
+        <div class="col-3 finishbtn"onclick="window.location.href='/wochenkochplan'">
+            <br>
+            <i class="fas fa-calendar-week"></i>
+            <p>Wochenkochplan</p>
+        </div>
+        <div class="col-3 bluebtn">{{--TODO href onclick="window.location.href='/einkaufsliste'"--}}            {{--TODO btn blue--}}
+            <br>
+            <i class="fas fa-list-alt"></i>
+            <p>Einkaufsliste</p>
+        </div>
+    </div>
 </div>
 @endsection
