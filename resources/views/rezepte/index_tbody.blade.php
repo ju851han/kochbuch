39248959935ee298fdea006e98bb28dd76e63d09
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-03 19:16:24 +0200

    delete comments

--- resources/views/rezepte/index_tbody.blade.php
+++ resources/views/rezepte/index_tbody.blade.php
@@ -1,8 +1,6 @@
 @foreach ($rezepte as $r)
     <tr id="{{$r->rID}}">
         <td>
-            {{--            <button id="btn_{{$r->rID}}" class="btn-primary" data-toggle="collapse" data-target="#tr_{{$r->rID}}">+
-                        </button>--}}
             <button id="btn_{{$r->rID}}" class="btn-primary" onclick="show_details({{$r->rID}});">+
             </button>
         </td>
@@ -23,25 +21,6 @@
             @endif
         </td>
     </tr>
-    {{-- <tr id="tr_{{$r->rID}}" class="collapse">
-         <td colspan="5">Kategorie: {{$r->kategorie}} <br>
-             <table class=" col-8 col-md-6 align-self-center">
-                 <thead>
-                 <tr>
-                     <th class="bg-white">Menge</th>
-                     <th class="bg-white">Zutat</th>
-                 </tr>
-                 </thead>
-                 <tbody class="background2ndTR">
-                 @foreach($r->zutats  as $zutat )
-                     <tr>
-                         <td> {{$r->zutats()->where('zutat_zName',$zutat->zName)->first()->pivot->menge}} {{$zutat->mengeneinheit}}</td>
-                         <td>{{ $zutat->zName }}</td>
-                     </tr>
-                 @endforeach
-                 </tbody>
-             </table>
-     </tr>--}}
 @endforeach
 
 <script>
