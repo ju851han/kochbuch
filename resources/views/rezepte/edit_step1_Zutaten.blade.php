b288811463500cb49a85b758776cf3f2f5bf26fe
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-04 14:13:18 +0200

    correction; form shows the right old selected items

--- resources/views/rezepte/edit_step1_Zutaten.blade.php
+++ resources/views/rezepte/edit_step1_Zutaten.blade.php
@@ -12,33 +12,35 @@
                 </nav>
                 <form method="post" action="/rezepte/edit_step2">
                     @csrf
-                    @foreach($r->zutats as $z)
-                        <div class="form-row">
-                            <input type="hidden" name="anz_rows" value="1" id="anz_rows">
-                            <div class="form-group col-5">
-                                <label for="menge">Menge</label>
-                                <div class="form-row">
-                                    <input id="menge_1" class="form-control col-11" name="menge_1" type="number"
-                                           min="0.5" max="1000000" step="0.5" onchange="calc_kosten(1)"
-                                           value="{{$r->zutats()->where('zutat_zName',$z->zName)->first()->pivot->menge}}"
-                                           required>
-                                    <output id="mengeneinheit_1" class="mengeneinheit col-1" name="mengeneinheit_1">
-                                    </output>
-                                </div>
-                            </div>
-                            <div class="form-group col-6 offset-1">
-                                <label for="zName">Zutat</label>
-                                <select name="zName_1" id="zName_1" class="form-control"
-                                        onchange="updateMengeneinheit(this.value,1);">
-                                    @foreach ($zutaten as $zutat)
-                                        {{--https://stackoverflow.com/questions/47996563/change-value-with-dropdown-in-laravel--}}
-                                        <option value="{{$zutat->zName}}"
-                                                @if($zutat->zName == $z->zName) selected @endif>{{$zutat->zName}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-                    @endforeach
+                    <?php
+                    $i=1;
+                    foreach ($r->zutats as $z) {
+                        echo "<div class='form-row'>";
+                        echo "<input type='hidden' name='anz_rows' value='" . $i . "' id='anz_rows'>";
+                        echo "<div class='form-group col-5'>";
+                        echo "<label for='menge'>Menge</label>";
+                        echo "<div class='form-row'>";
+                        echo "<input id='menge_" . $i . "' class='form-control col-11' name='menge_" . $i . "' type='number' min='0.5' max='1000000' step='0.5' onchange='calc_kosten(" . $i . ")' value='".$r->zutats()->where('zutat_zName',$z->zName)->first()->pivot->menge."' required>";
+                        echo "<output id='mengeneinheit_" . $i . "' class='mengeneinheit col-1' name='mengeneinheit_" . $i . "'>";
+                        echo "</output>";
+                        echo "</div>";
+                        echo "</div>";
+                        echo "<div class='form-group col-6 offset-1'>";
+                        echo "<label for='zName'>Zutat</label>";
+                        echo "<select name='zName_" . $i . "' id='zName_" . $i . "' class='form-control' onchange='updateMengeneinheit(this.value," . $i . ");'>";
+                        foreach ($zutaten as $zutat) {
+                            if ($zutat->zName == $z->zName) {
+                                echo "<option value='".$zutat->zName."' selected>".$zutat->zName."</option>";
+                            } else {
+                                echo "<option value='".$zutat->zName."'>".$zutat->zName."</option>";
+                            }
+                        }
+                        echo "</select>";
+                        echo "</div>";
+                        echo "</div>";
+                        $i++;
+                    }
+                    ?>
                     <input class="form-control" type="hidden" id="kostenjePortion" name="kostenjePortion">
                     <br>
                     <div class="row justify-content-end">
