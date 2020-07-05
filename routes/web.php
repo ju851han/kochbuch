b9d0f2227a76606fb4619615ef3b0629a58be507
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-05 14:04:11 +0200

    edit Update Rezept routes

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,6 @@ Route::get('/rezepte/{rID}','RezeptController@show')->name('rezepte.show');
 //Update Rezept
 Route::get('rezepte/{rID}/edit_step1','RezeptController@edit')->name('rezepte.edit_step1_Zutaten');
 Route::post('/rezepte/{rID}/edit_step2','RezeptController@edit_step2')->name('rezepte.edit_step2_Rezept');
-Route::post('/rezepte/{rID}/edit_step3','RezeptController@edit_step3')->name('rezepte.edit_step3_overview');
 Route::get('/rezepte/{rID}/update','RezeptController@update')->name('rezepte.update');
 
 //Delete Rezept
