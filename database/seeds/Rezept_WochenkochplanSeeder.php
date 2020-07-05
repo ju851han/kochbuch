bdbe8bd83caa449a4f40c1ac9962e5aea324a4de
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-05 17:41:13 +0200

    correction wochenkochplan_id

--- database/seeds/Rezept_WochenkochplanSeeder.php
+++ database/seeds/Rezept_WochenkochplanSeeder.php
@@ -13,20 +13,20 @@ class Rezept_WochenkochplanSeeder extends Seeder
     public function run()
     {
         DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('1','1','1');");
-        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('2','2','1');");
-        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('3','3','1');");
-        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('4','4','1');");
-        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('5','5','1');");
-        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('6','6','1');");
-        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('7','7','1');");
+        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('2','2','2');");
+        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('3','3','3');");
+        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('4','4','4');");
+        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('5','5','5');");
+        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('6','6','6');");
+        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('7','7','7');");
 
-        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('8','7','2');");
-        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('9','6','2');");
-        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('10','5','2');");
-        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('11','4','2');");
-        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('12','3','2');");
-        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('13','2','2');");
-        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('14','1','2');");
+        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('8','7','8');");
+        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('9','6','9');");
+        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('10','5','10');");
+        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('11','4','11');");
+        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('12','3','12');");
+        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('13','2','13');");
+        DB::insert("INSERT INTO rezept_wochenkochplan (id, rezept_rID, wochenkochplan_id)VALUES ('14','1','14');");
 
     }
 }
