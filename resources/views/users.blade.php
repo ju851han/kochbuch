1f08fea4693527f5a2b9d3d12163431d1f92067d
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-06 11:28:57 +0200

    add btn class

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <section class="col-12 col-md-8 offset-md-2">
-                <h2>Nutzer <i class='fas fa-user'></i></h2>
+                <h2><i class='fas fa-user'></i> Nutzer</h2>
                 <table class="table">
                     <thead class="thead_orangered">
                     <tr>
@@ -21,7 +21,7 @@
                             <td> {{  $u->email }}</td>
                             <td> {{  $u->role }}</td>
                             <td>
-                                <button class="normalbtn"
+                                <button class="normalbtn btn"
                                         onclick="window.location.href='/users/{{$u->id}}/destroy'"
                                         title="Benutzer löschen"><i class="fas fa-trash-alt btn_i"></i></button>
                             </td>
