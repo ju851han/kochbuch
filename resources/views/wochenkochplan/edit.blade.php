beb814d957e3a77328c1259abfd8ed210cf30707
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-07 22:33:00 +0200

    add button hrefs

--- resources/views/wochenkochplan/edit.blade.php
+++ resources/views/wochenkochplan/edit.blade.php
@@ -10,7 +10,6 @@
                     <tr>
                         <th>Wochentag</th>
                         <th>geplantes Rezept</th>
-                        <th>Zeit</th>
                         <th>Kosten</th>
                         <th>Portion</th> {{--TODO Portion eingeben ->  Kosten berechnen lassen --}}
                     </tr>
@@ -21,21 +20,22 @@
 
                             <td>{{$wochenkochplan[$i]->wochentag}}</td>
                             @if (is_null($rezepte[$i]))
-                                <td colspan="4">
-                                    <button class="normalbtn btn"><i class="material-icons btn_i">add_circle</i> Rezept hinzufügen</button>
+                                <td colspan="3">
+                                    <button class="normalbtn btn"
+                                            onclick="window.location.href='/wochenkochplan/{{$wochenkochplan[$i]->id}}/addRezept'">
+                                        <i class="material-icons btn_i">add_circle</i> Rezept hinzufügen
+                                    </button>
                                 </td>
                             @else
                                 <td>
                                     {{$rezepte[$i]->rName}}
                                 </td>
-                                <td>
-                                    {{$rezepte[$i]->zeit}} min
-                                </td>
                                 <td class="text-center">
                                     {{ number_format($rezepte[$i]->kostenjePortion, 2,",", "." )}} €
                                 </td>
                                 <td>
-                                    <input type="number" min="1.0" step="0.5" max="100" value="{{$wochenkochplan[$i]->portion}}">
+                                    <input type="number" min="1.0" step="0.5" max="100"
+                                           value="{{$wochenkochplan[$i]->portion}}">
                                 </td>
                             @endif
 
@@ -43,13 +43,13 @@
                     @endfor
                     </tbody>
                 </table>
-                <br>
+                <p id="gesamtkosten">Gesamtkosten betragen:</p>
                 <button class="normalbtn btn">Einkaufsliste erstellen</button>
-                <br>
-                <br>
-                <button type="reset" class="normalbtn btn" value="Entleeren">Entleeren</button>
+                <button type="reset" class="normalbtn btn" value="Entleeren" onclick="window.location.href='/wochenkochplan/destroy'">Entleeren</button>
                 <!-- reset = Formulardaten werden gelöscht-->
-                <button type="submit" class="normalbtn btn"   onclick="window.location.href='/wochenkochplan/update'" value="Speichern">Speichern</button>
+                <button type="submit" class="normalbtn btn" onclick="window.location.href='/wochenkochplan/update'"
+                        value="Speichern">Speichern
+                </button>
             </section>
         </div>
     </div>
