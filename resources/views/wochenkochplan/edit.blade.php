e1abdc85b8a75e1d9e5840f909fbe85edce99abd
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-07 21:02:10 +0200

    update view; add formatting classes

--- resources/views/wochenkochplan/edit.blade.php
+++ resources/views/wochenkochplan/edit.blade.php
@@ -6,47 +6,49 @@
             <section class="col-12 col-md-8 offset-md-2">
                 <h2>Wochenkochplan <i class="fas fa-calendar-week"></i></h2>
                 <table class="table">
-                    <thead>
+                    <thead class="thead_orangered">
                     <tr>
                         <th>Wochentag</th>
                         <th>geplantes Rezept</th>
                         <th>Zeit</th>
-                         <th>Kosten</th>
-                         <th>Portion</th> {{--TODO Portion eingeben ->  Kosten berechnen lassen --}}
+                        <th>Kosten</th>
+                        <th>Portion</th> {{--TODO Portion eingeben ->  Kosten berechnen lassen --}}
                     </tr>
                     </thead>
-                    <tbody>
-                    @foreach( $wochentag as $w)
+                    <tbody class="background2ndTR">
+                    @for ($i = 0; $i < count($wochenkochplan); $i++)
                         <tr>
-                            <td>{{$w->wochentag}}</td>
-                            @if (is_null($w->rezepts()->where('wochenkochplan_id',$w->id)->first()))
+
+                            <td>{{$wochenkochplan[$i]->wochentag}}</td>
+                            @if (is_null($rezepte[$i]))
                                 <td colspan="4">
-                                    <?php echo"hi"?>
-                                    <button>Rezept hinzufügen</button>
+                                    <button class="normalbtn btn"><i class="material-icons btn_i">add_circle</i> Rezept hinzufügen</button>
                                 </td>
                             @else
                                 <td>
-                                    {{$w->rezepts()->where('wochenkochplan_id',$w->id)->first()->rName}}
+                                    {{$rezepte[$i]->rName}}
                                 </td>
                                 <td>
-                                    {{$w->rezepts()->where('wochenkochplan_id',$w->id)->first()->zeit}} min
+                                    {{$rezepte[$i]->zeit}} min
+                                </td>
+                                <td class="text-center">
+                                    {{ number_format($rezepte[$i]->kostenjePortion, 2,",", "." )}} €
                                 </td>
                                 <td>
-                                    {{$w->rezepts()->where('wochenkochplan_id',$w->id)->first()->kostenjePortion}} €
-                                </td>
-                                <td>
-
+                                    <input type="number" min="1.0" step="0.5" max="100" value="{{$wochenkochplan[$i]->portion}}">
                                 </td>
                             @endif
+
                         </tr>
-                    @endforeach
+                    @endfor
                     </tbody>
                 </table>
                 <br>
                 <button class="normalbtn btn">Einkaufsliste erstellen</button>
                 <br>
                 <br>
-                <button type="reset" class="normalbtn btn" value="Entleeren">Entleeren</button> <!-- reset = Formulardaten werden gelöscht-->
+                <button type="reset" class="normalbtn btn" value="Entleeren">Entleeren</button>
+                <!-- reset = Formulardaten werden gelöscht-->
                 <button type="submit" class="normalbtn btn" value="Speichern">Speichern</button>
             </section>
         </div>
