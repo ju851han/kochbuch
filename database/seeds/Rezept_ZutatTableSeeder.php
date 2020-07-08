e40a120d59ebcef96ebfa2e413d28fa713c01486
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-08 21:05:36 +0200

    correction seeder

--- database/seeds/Rezept_ZutatTableSeeder.php
+++ database/seeds/Rezept_ZutatTableSeeder.php
@@ -44,28 +44,73 @@ class Rezept_ZutatTableSeeder extends Seeder
         DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('27','6','Olivenöl','1');");
         DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('28','6','Senf','50');");
 
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('29','6','Reis','450');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('30','6','Wasser','600');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('31','6','Essig','50');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('32','6','Zucker','10');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('33','6','Salz','1');");
 
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('34','7','Olivenöl','1');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('35','7','Zitrone','50');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('36','7','Apfel','80');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('37','7','Pfeffer','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('29','7','Reis','450');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('30','7','Wasser','600');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('31','7','Essig','50');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('32','7','Zucker','10');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('33','7','Salz','1');");
 
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('38','8','Apfel','50');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('39','8','Haselnüsse','15');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('40','8','Zimt','1');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('41','8','Toast','1');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('42','8','Quark','40');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('34','8','Olivenöl','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('35','8','Zitrone','50');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('36','8','Apfel','80');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('37','8','Pfeffer','1');");
 
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('43','9','Apfel','1');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('44','9','Erdbeeren','30');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('45','9','Wahlnüsse','5');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('46','9','Quark','100');");
-        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('47','9','Erdbeereis','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('38','9','Apfel','50');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('39','9','Haselnüsse','15');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('40','9','Zimt','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('41','9','Toast','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('42','9','Quark','40');");
+
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('43','10','Apfel','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('44','10','Erdbeeren','30');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('45','10','Wahlnüsse','5');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('46','10','Quark','100');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('47','10','Erdbeereis','1');");
+
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('48','11','Hähnchen','220');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('49','11','Orange','2');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('50','11','Olivenöl','2');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('51','11','Paprikapulver','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('52','11','Curry','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('53','11','Salz','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('54','11','Pfeffer','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('55','11','Paprika','200');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('56','11','Aprikose','100');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('57','11','Couscous','140');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('58','11','Petersilie','20');");
+
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('59','12','Zwiebel','2');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('60','12','Knoblauch','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('61','12','Karotten','200');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('62','12','Zucchini','500');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('63','12','Olivenöl','2');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('64','12','Tomatenmark','80');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('65','12','Apfelsaft','150');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('66','12','Tomaten','1600');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('67','12','Salz','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('68','12','Pfeffer','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('69','12','Nudeln','600');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('70','12','Wahlnüsse','100');");
+
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('71','13','Salz','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('72','13','Nudeln','160');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('73','13','Brokkoli','250');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('74','13','Karotten','100');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('75','13','Zwiebel','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('76','13','Knoblauch','5');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('77','13','Wahlnüsse','30');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('78','13','Olivenöl','2');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('79','13','Curry','2');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('80','13','Pfeffer','2');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('81','13','Sojasauce','1');");
+
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('82','14','Brokkoli','400');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('83','14','Kartoffeln','500');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('84','14','Salz','1');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('85','14','Milch','200');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('86','14','Frischkäse','120');");
+        DB::insert("INSERT INTO rezept_zutat (id, rezept_rID, zutat_zName, menge)VALUES ('87','14','Pfeffer','1');");
 
     }
 }
--- database/seeds/RezeptsTableSeeder.php
+++ database/seeds/RezeptsTableSeeder.php
@@ -17,10 +17,12 @@ class RezeptsTableSeeder extends Seeder
         DB::insert("INSERT INTO rezepts (rName,kategorie,zeit,kostenjePortion,zubereitung, created_at, updated_at) VALUES ('Fischstäbchen','Fisch','15','3.00','Fischstäbchen nach Packungsanleitung zubereiten und genießen.',NOW(),NOW());");
         DB::insert("INSERT INTO rezepts (rName,kategorie,zeit,kostenjePortion,zubereitung, created_at, updated_at) VALUES ('Pommes','Snacks','30','5.00','Kartoffeln waschen, schälen und in Streifen schneiden. Anschließend mit etwas Öl und Gewürzen vermischen. Auf einem Backblech ausbreiten und für etwa 25-30 minuten bei 200°C Umluft in den Backofen. Anschließend mit Mayo servieren, fertig.',NOW(),NOW());");
         DB::insert("INSERT INTO rezepts (rName,kategorie,zeit,kostenjePortion,zubereitung, created_at, updated_at) VALUES ('Gemischter Salat','Vegetarisch','25','5.00','Die Zwiebeln in etwas von dem Olivenöl anbraten, wenn die Zwiebeln glasig sind, die geschnittene Paprika hinzufügen. Beides wird in der Pfanne mit etwas Salz, Pfeffer und braunem Zucker geröstet. In der Zwischenzeit den Salat waschen, danach die Karotten in Scheiben und die Cherrytomaten in Viertel schneiden. Die Zwiebel-/Paprikamischung kann derweil etwas abkühlen, damit der Salat beim Zusammenmischen nicht zerfällt. Den Rest vom Olivenöl nun mit dem Senf, dem Sambal Oelek, dem Essig, dem Honig und den Kräutern gut verrühren - das Dressing mit Salz und Pfeffer gut abschmecken. Das Dressing drüber und gut vermischen.',NOW(),NOW());");
-        DB::insert("INSERT INTO rezepts (rName,kategorie,zeit,kostenjePortion,zubereitung, created_at, updated_at) VALUES ('Sushi-Reis','Vegetarisch','35','5.00','Reis in einem Sieb gründlich waschen, bis das Wasser klar bleibt, anschließend gut abtropfen lassen. Den Reis mit der angegebenen Menge Wasser in den Topf geben und ca. 20 Min. ruhen lassen.
+        DB::insert("INSERT INTO rezepts (rName,kategorie,zeit,kostenjePortion,zubereitung, created_at, updated_at) VALUES ('Sushi-Reis','Vegetarisch','35','5.00','Reis in einem Sieb gründlich waschen, bis das Wasser klar bleibt, anschließend gut abtropfen lassen.
+Den Reis mit der angegebenen Menge Wasser in den Topf geben und ca. 20 Min. ruhen lassen.
 Dann den Topf mit einem gut sitzenden Deckel schließen und den Inhalt langsam erwärmen. Die Hitze dann auf die größte Stufe stellen und zum Kochen bringen. Nun wieder auf die kleinste Stufe stellen und den Reis ca. 10 Minuten ausquellen lassen.
 Den Topf vom Herd nehmen, ein gefaltetes Küchentuch unter den Deckel legen und den Reis noch 10 Min. nachquellen lassen. Die Würzzutaten in einem Topf mischen und erwärmen, bis Zucker und Salz sich ganz aufgelöst haben.
 Anschließend die Würzmischung über den Reis geben und miteinander vermengen. ',NOW(),NOW());");
+
         DB::insert("INSERT INTO rezepts (rName,kategorie,zeit,kostenjePortion,zubereitung, created_at, updated_at) VALUES ('Apfel-Möhren-Salat','Vegetarisch','35','5.00',' Zubereitungsschritte
 1.
 
@@ -61,5 +63,71 @@ Quarkcreme mit Eis und Obst Zubereitung Schritt 4
 
 Apfel und Erdbeeren unterheben, mit Pistazien bestreuen und sofort servieren.
 ',NOW(),NOW());");
+
+        DB::insert("INSERT INTO rezepts (rName,kategorie,zeit,kostenjePortion,zubereitung, created_at, updated_at) VALUES ('Hähnchenspieße mit Couscous','Fleisch','40','5.00','1.
+Hähnchenfilet abspülen, mit Küchenpapier trockentupfen und in etwa 2 cm große Würfel schneiden.
+2.
+Orangen halbieren und auspressen.
+3.
+Vom Orangensaft 3 EL abnehmen und mit 1 EL Rapsöl und der Konfitüre in einer Schüssel verrühren. Mit Paprika- und Currypulver und Salz und Pfeffer würzen.
+4.
+Hähnchenfilet unter die Mischung heben und zugedeckt im Kühlschrank ziehen lassen (marinieren).
+5.
+Paprikaschote vierteln, entkernen und waschen. 3 Viertel in ca. 2 cm große Würfel schneiden, das letzte Viertel sehr klein würfeln und in einer kleinen Schüssel beiseitestellen.
+6.
+Aprikosen waschen, halbieren, entsteinen und in Spalten schneiden.
+7.
+Den restlichen Orangensaft mit Wasser auf 220 ml auffüllen und in einem kleinen Topf aufkochen. Couscous mit etwas Salz und 1 Prise Curry unterrühren, vom Herd nehmen und etwa 15 Minuten quellen lassen.
+8.
+Inzwischen Petersilie waschen, Blätter abzupfen und hacken.
+9.
+Hähnchenfilet aus der Marinade nehmen, etwas abtropfen lassen und abwechselnd mit Paprikastücken und Aprikosenspalten auf 4 Schaschlikspieße stecken.
+10.
+Das restliche Öl in einer flachen Pfanne erhitzen. Die Spieße darin bei mittlerer Hitze unter häufigem Wenden braten, bis das Fleisch rundherum goldbraun ist; das dauert 8-10 Minuten. Marinade dazugeben und aufkochen lassen.
+11.
+Petersilie und die feingewürfelte Paprikaschote unter den Couscous heben. Mit Salz und Pfeffer abschmecken und zu den Spießen servieren.
+',NOW(),NOW());");
+        DB::insert("INSERT INTO rezepts (rName,kategorie,zeit,kostenjePortion,zubereitung, created_at, updated_at) VALUES ('Nudeln mit Nuss-Tomaten-Sauce','Vegetarisch','35','5.00','1.
+Zwiebeln und Knoblauch schälen und klein würfeln. Möhren waschen, putzen, schälen und auf einer Vierkantreibe grob raspeln.
+2.
+Zucchini putzen, waschen und ebenfalls grob raspeln.
+3.
+Das Öl in einem weiten Topf erhitzen. Zwiebeln und Knoblauch darin bei mittlerer Hitze glasig dünsten. Möhren- und Zucchiniraspel zugeben und etwa 1 Minute mit andünsten.
+4.
+Tomatenmark zufügen und kurz mitrösten. Apfelsaft und passierte Tomaten unterrühren. Mit Salz, Pfeffer und Oregano würzen. Bei kleiner Hitze im offenen Topf alles etwa 20 Minuten köcheln lassen, bis die Sauce sämig wird.
+5.
+Inzwischen die Nudeln nach Packungsanleitung in reichlich kochendem Salzwasser bissfest garen. Walnüsse in einem Blitzhacker fein zerkleinern und in einer beschichteten Pfanne anrösten, bis sie anfangen zu duften.
+6.
+Walnüsse zum Schluss unter die Tomatensauce mischen, nochmals mit Salz und Pfeffer abschmecken. Nudeln abgießen, abtropfen lassen und mit der Sauce servieren.
+',NOW(),NOW());");
+
+        DB::insert("INSERT INTO rezepts (rName,kategorie,zeit,kostenjePortion,zubereitung, created_at, updated_at) VALUES ('Vegetarische Nudelpfanne','Vegetarisch','30','5.10','1.
+In einem Topf 500 ml Salzwasser zum Kochen bringen, die Mie-Nudeln hineingeben und einmal aufkochen. Bei kleiner Hitze zugedeckt etwa 5 Minuten ziehen lassen, dann in einem Sieb abgießen und abtropfen lassen.
+2.
+Brokkoli waschen, putzen und die Röschen vom Stiel schneiden. Den Stiel schälen und fein hacken.
+3.
+Möhre waschen, putzen, schälen und auf einem Gemüsehobel oder mit einem Sparschäler in dünne Streifen hobeln.
+4.
+Frühlingszwiebel waschen, putzen und in feine Ringe schneiden. Knoblauchzehe schälen und fein hacken.
+5.
+Cashewkerne mit Öl in einem Wok oder einer großen tiefen Pfanne erhitzen. Möhren, Brokkolistiele, Knoblauch und 1 Msp. Gewürzmischung dazugeben und unter Rühren etwa 5 Minuten braten.
+6.
+Frühlingszwiebeln und Brokkoliröschen zufügen und alles weitere 2 Minuten unter Rühren braten. Mit Sojasauce abschmecken.
+7.
+Die Sprossen kalt abspülen, gut abtropfen lassen und mit den gekochten Nudeln unter das Gemüse mischen. Unter ständigem Rühren alles weitere 2 Minuten braten und sofort servieren.
+',NOW(),NOW());");
+        DB::insert("INSERT INTO rezepts (rName,kategorie,zeit,kostenjePortion,zubereitung, created_at, updated_at) VALUES ('Brokkoli-Rahm-Kartoffeln','Vegetarisch','25','5.00','1.
+Brokkoli waschen und in Röschen zerteilen. Die Stiele schälen und in kleine Würfel schneiden. Kartoffeln waschen, schälen und ebenfalls würfeln.
+2.
+In einem Topf 150 ml Wasser mit 1/2 TL Salz aufkochen lassen. Kartoffeln und Brokkolistiele dazugeben und zugedeckt 10–15 Minuten bissfest garen.
+3.
+Inzwischen die Kürbiskerne mit einem großen Messer grob hacken und in einer beschichteten Pfanne kurz anrösten.
+4.
+Brokkoliröschen und Milch zu den Kartoffeln geben. Einige Minuten weiterdünsten, bis alles gar ist.
+5.
+Frischkäse unterrühren und schmelzen lassen. Mit Salz und Pfeffer würzen. (Wenn die Flüssigkeit zu dick ist, noch etwas Wasser zugeben.) Kürbiskerne darüberstreuen und die Brokkoli-Rahm-Kartoffeln servieren.
+',NOW(),NOW());");
+
+
     }
 }
