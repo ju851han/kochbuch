c4c99925d22f0f400378053be684da585f1f3aa9
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-08 20:20:19 +0200

    add error validation alerts

--- resources/views/kochbuecher/create_step1_Kochbuch.blade.php
+++ resources/views/kochbuecher/create_step1_Kochbuch.blade.php
@@ -10,11 +10,20 @@
                         <li class="breadcrumb-item active" aria-current="page">1. Name des Kochbuches eingeben</li>
                     </ol>
                 </nav>
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
                 <form method="post" action="/kochbuecher/create_step3">
                     @csrf
                     <div class="form-group">
                         <label for="kName">Name des Kochbuchs:</label>
-                        <input class="form-control" id="kName" name="kName" type="text" minlength="2" required>
+                        <input class="form-control" id="kName" name="kName" type="text" minlength="2" maxlength="125" required>
                     </div>
                     <br>
                     <div class="row justify-content-end">
