7966aece78eff6fe250ff0029902d46c9f9153c4
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-08 20:45:46 +0200

    add check eMail method

--- resources/views/kontakt.blade.php
+++ resources/views/kontakt.blade.php
@@ -8,10 +8,10 @@
                 <p>Haben Sie Fragen oder wollen Sie ein Feedback zur Web-Applikation "Kochplan" geben? <br>
                    Dann füllen Sie einfach das folgende Formularfelder aus:</p>
                 {{--TODO https://html.form.guide/email-form/html-email-form/--}}
-                <form method="POST" action="mailto:julia.hansi@htwg-konstanz.de" enctype="text/plain">
+                <form method="POST" action="mailto:julia.hansi@htwg-konstanz.de" enctype="text/plain" onsubmit="check_email()">
                     <div class="form-group">
                         <label for="eMail">Ihre E-Mail Adresse:</label>
-                        <input class="form-control" id="eMail" name="eMail" type="email" required></div>
+                        <input class="form-control" id="eMail" name="eMail" type="email" onchange="check_email()" required></div>
                     <div class="form-group">
                         <label for="betreff">Betreff:</label>
                         <input class="form-control" id="betreff" name="betreff" type="text" size="25" required></div>
@@ -19,10 +19,17 @@
                         <textarea class="form-control" id="text" name="text" rows="5" cols="30"></textarea></div>
                     <br>
                     <input type="submit" class="normalbtn btn" value="Senden">
-                    <input type="reset" class="abortbtn btn" value="Abbrechen">
+                    <input type="reset" class="abortbtn btn" onclick="window.location.href='/'" value="Abbrechen">
                 </form>
             </section>
         </div>
     </div>
+    <script>
+        function check_email(){
+            if(!$('#eMail').val().includes('.')){
+                alert('Bitte geben Sie eine korrekte E-Mail-Addresse ein.')
+            }
+        }
+    </script>
     <!-- Quelle https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit-->
 @endsection
