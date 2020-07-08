5bee3ad494f2f7bbe784dc72d26311d7cec105cd
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-08 10:06:42 +0200

    add jquery function sum gesamtkosten

--- resources/views/wochenkochplan/edit.blade.php
+++ resources/views/wochenkochplan/edit.blade.php
@@ -5,23 +5,24 @@
         <div class="row">
             <section class="col-12 col-md-8 offset-md-2">
                 <h2>Wochenkochplan <i class="fas fa-calendar-week"></i></h2>
+                <form method="post" action="/wochenkochplan/update">
                 <table class="table">
                     <thead class="thead_orangered">
                     <tr>
                         <th>Wochentag</th>
                         <th>geplantes Rezept</th>
-                        <th>Kosten</th>
+                        <th>Kosten je Portion</th>
                         <th>Portion</th> {{--TODO Portion eingeben ->  Kosten berechnen lassen --}}
                     </tr>
                     </thead>
-                    <tbody class="background2ndTR">
+                    <tbody class="background2ndTR" onchange="updateGesamtkosten();">
                     @for ($i = 0; $i < count($wochenkochplan); $i++)
                         <tr>
 
                             <td>{{$wochenkochplan[$i]->wochentag}}</td>
                             @if (is_null($rezepte[$i]))
                                 <td colspan="3">
-                                    <button class="normalbtn btn"
+                                    <button class="normalbtn btn" id="btn_{{$wochenkochplan[$i]->id}}"
                                             onclick="window.location.href='/wochenkochplan/{{$wochenkochplan[$i]->id}}/addRezept'">
                                         <i class="material-icons btn_i">add_circle</i> Rezept hinzufügen
                                     </button>
@@ -30,17 +31,20 @@
                                 <td>
                                     {{$rezepte[$i]->rName}}
                                 </td>
-                                <td class="text-center">
+                                <param id="kosten_{{$wochenkochplan[$i]->id}}" name="kosten_{{$wochenkochplan[$i]->id}}" value="{{$rezepte[$i]->kostenjePortion}}">
+                                <td  class="text-center">
                                     {{ number_format($rezepte[$i]->kostenjePortion, 2,",", "." )}} €
-                                </td>
+                                        </td>
                                 <td>
-                                    <input type="number" min="1.0" step="0.5" max="100"
+                                    <label type="hidden" for="portion_{{$wochenkochplan[$i]->id}}"></label>
+                                    <input id="portion_{{$wochenkochplan[$i]->id}}" name="portion_{{$wochenkochplan[$i]->id}}" type="number" min="1.0" step="0.5" max="100"
                                            value="{{$wochenkochplan[$i]->portion}}">
                                 </td>
                             @endif
 
                         </tr>
                     @endfor
+
                     </tbody>
                 </table>
                 <p id="gesamtkosten">Gesamtkosten betragen:</p>
@@ -50,7 +54,22 @@
                 <button type="submit" class="normalbtn btn" onclick="window.location.href='/wochenkochplan/update'"
                         value="Speichern">Speichern
                 </button>
+                </form>
             </section>
         </div>
     </div>
+    <script>
+        function updateGesamtkosten() {
+            var sum=0;
+           @foreach($wochenkochplan as $w)
+                if($('#btn_{{$w->id}}').length===0){
+                    console.log({{$w->id}});
+                    console.log("€ "+$('#kosten_{{$w->id}}').val());
+                    sum = sum + ($('#kosten_{{$w->id}}').val() * $('#portion_{{$w->id}}').val());
+
+                }
+         @endforeach
+            $('#gesamtkosten').replaceWith("<p id=\"gesamtkosten\">Gesamtkosten betragen: €"+(Math.round(sum * 100) / 100).toFixed(2)+"</p>");
+        }
+    </script>
 @endsection
