ca057d4f329b6169f4112930b2531b8ee0745a2d
Julia Hansi <ju851han@htwg-konstanz.de>
2020-07-08 13:23:10 +0200

    add einkaufsliste

---
+++ resources/views/einkaufsliste.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <section class="col-12 col-md-8 offset-md-2">
+                <h2>Einkaufsliste</h2>
+                
+                <p>Backartikel</p>
+                
+                <p>Backwaren</p>
+
+                <p>Fisch & Meeresfrüchte</p>
+
+                <p>Fleisch</p>
+
+                <p>Gemüse</p>
+
+                <p>Gewürze</p>
+
+                <p>Grundnahrungsprodukt</p>
+
+
+                <p>Milchprodukt</p>
+
+
+                <p>Obst & Gemüse</p>
+
+
+            </section>
+        </div>
+    </div>
+    @endsection
